8779d4f03c1c1fb26235b8b07ae865cab3d18bfc
vfaddey <vfaddey@yandex.ru>
2024-09-06 21:19:31 +0300

    new main file

--- config.py
+++ config.py
@@ -4,12 +4,16 @@ auth_server = "http://31.129.54.119"
 secret_key = "secret_key"
 
 dt_format = '%d/%m/%Y %H:%M:%S'
-token_lifetime = 36000
+token_lifetime = 360000
 
 webhook_port = 8000
 webhook_ssl_cert = None
 webhook_ssl_priv = None
 
-archetypes = [{"title": "Strength", "chars": []},
-              {"title": "Dexterity", "chars": []},
-              {"title": "Intelligence", "chars": []}]
+archetypes = [{"title": "Strength"},
+              {"title": "Dexterity"},
+              {"title": "Intelligence"}]
+
+character_types = [{'name': 'main'},
+                   {'name': 'collection'},
+                   {'name': 'secondary'}]
---
+++ main.py
@@ -0,0 +1,97 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy import select
+from starlette.middleware.cors import CORSMiddleware
+
+from auth.models import User
+from auth.user_service import get_current_user
+from chat.router import router as chat_router
+from chat.websocket import router as chat_websocket_router
+from database import AsyncSessionFactory
+from game_logic.models import CharacterArchetype, Character
+
+from schemas import CreateCharacterSchema
+
+app = FastAPI()
+
+app.include_router(chat_router)
+app.include_router(chat_websocket_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/archetypes')
+async def servers_handler(user: User = Depends(get_current_user)):
+    response = {
+        "message": "List of available archetypes",
+        "archetypes": []
+    }
+    async with AsyncSessionFactory() as db:
+        result = await db.execute(select(CharacterArchetype))
+        auth_sessions = result.scalars().all()
+        for archetype in auth_sessions:
+            response['archetypes'].append(archetype.serialize())
+
+        return response
+
+
+@app.get('/summary')
+async def servers_handler(user: User = Depends(get_current_user)):
+    response = {"message": "General summary",
+                "server_info": None,
+                "user_info": None,
+                "character_info": None}
+
+    async with AsyncSessionFactory() as db:
+        user = await db.merge(user)
+
+        response["user_info"] = {"username": user.username,
+                                 "email": user.email}
+
+        for character in user.characters:
+            if character.character_type.name == 'main':
+                response["character_info"] = {"name": character.name,
+                                              "archetype": character.archetype_id}
+
+        return response
+
+
+@app.post('/create_character')
+async def servers_handler(create_character: CreateCharacterSchema,
+                          user: User = Depends(get_current_user)):
+    response = {
+        "message": "The main character has been successfully created!",
+        "character_info": None
+    }
+
+    async with AsyncSessionFactory() as db:
+        user = await db.merge(user)
+
+        for character in user.characters:
+            if character.character_type.name == 'main':
+                response["message"] = "The main character has already been created!"
+                response["character_info"] = CreateCharacterSchema(name=character.name,
+                                                                   archetype_id=character.archetype_id)
+
+        if not response["character_info"]:
+            result = await db.execute(select(CharacterArchetype).where(CharacterArchetype.id == create_character.archetype_id))
+            existing_character = result.scalars().first()
+            if not existing_character:
+                response["message"] = "There is no archetype with such an id!"
+                return response
+
+            character = Character(name=create_character.name,
+                                  character_type_id=1,
+                                  archetype_id=create_character.archetype_id,
+                                  user_id=user.id)
+            user.characters.append(character)
+            await db.commit()
+            response["character_info"] = create_character
+
+        return response
