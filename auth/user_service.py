89a1e42441231619971b404c1be61adb29f7820b
vfaddey <vfaddey@yandex.ru>
2024-09-08 17:57:09 +0300

    убрал await (ошибка)

--- auth/user_service.py
+++ auth/user_service.py
@@ -96,7 +96,7 @@ async def check_remote_auth_token(token: str):
                                     email=data['user']['email'],
                                     create_date=datetime.strptime(data['user']['create_date'],
                                                                   config.dt_format))
-                        await db.add(user)
+                        db.add(user)
 
                     user.auth_sessions.append(auth_session)
                     await db.commit()
