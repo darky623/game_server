236497c6e8bb32eecc32f25f98046e0df8661856
vlad Amirov <vladamirov@list.ru>
2024-09-11 00:03:25 +0300

    router biome create

---
+++ bioms/router_biome.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+
+from bioms.biome_service import BiomeService
+from database import AsyncSessionFactory
+
+from schemas import BiomeSchema, BiomeCreateSchema
+router = APIRouter(prefix="/biome")
+biome_service = BiomeService(AsyncSessionFactory)
+
+
+@router.post('', response_model=BiomeSchema)
+async def create_biome(biome: BiomeCreateSchema):
+    return await biome_service.create_biome(biome)
+
+
+@router.get('/{biome_id}', response_model=BiomeSchema)
+async def get_biome(biome_id: int):
+    return await biome_service.get_biome(biome_id)
+
+
+@router.get('', response_model=list[BiomeSchema])
+async def get_all_biomes():
+    return await biome_service.get_biomes()
+
+
+@router.patch('/{biome_id}', response_model=BiomeSchema)
+async def update_biome(biome_id: int, biome: BiomeCreateSchema):
+    return await biome_service.update_biome(biome_id, biome)
+
+
+@router.delete('/{biome_id}')
+async def delete_biome(biome_id: int):
+    await biome_service.delete_biome(biome_id)
