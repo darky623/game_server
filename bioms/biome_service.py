9b7ac84f437796088ef2d9debb42b9ca6568369a
Vlad_work_oficie <vladamirov@list.ru>
2024-09-11 10:33:36 +0300

    try except add

--- bioms/biome_service.py
+++ bioms/biome_service.py
@@ -1,4 +1,6 @@
+from fastapi import HTTPException
 from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 
 from bioms.schemas import BiomeCreateSchema, BiomeSchema
 from bioms.models import Biome
@@ -10,44 +12,73 @@ class BiomeService:
 
     async def create_biome(self, biome: BiomeCreateSchema) -> BiomeSchema:
         async with self.session_factory() as session:
-            biome = Biome(**biome.dict())
-            session.add(biome)
-            await session.commit()
-            await session.refresh(biome)
-            return biome
+            try:
+                new_biome = Biome(**biome.dict())
+                session.add(new_biome)
+                await session.commit()
+                return new_biome
+            except SQLAlchemyError as e:
+                session.rollback()
+                raise HTTPException(500, "Error creating biome") from e
 
     async def get_biome(self, biome_id: int) -> BiomeSchema:
         async with self.session_factory() as session:
-            result = await session.execute(select(Biome).where(Biome.id == biome_id))
-            biome = result.scalars().first()
-            return biome
+            try:
+                result = await session.execute(
+                    select(Biome).where(Biome.id == biome_id)
+                )
+                biome = result.scalars().first()
+                if not biome:
+                    raise HTTPException(404, "Biome not found")
+                return biome
+            except SQLAlchemyError as e:
+
+                raise HTTPException(500, "Error getting biome") from e
 
     async def get_biomes(self) -> list[BiomeSchema]:
         async with self.session_factory() as session:
-            result = await session.execute(select(Biome))
-            biomes = result.scalars().all()
-            return biomes
+            try:
+                result = await session.execute(select(Biome))
+                biomes = result.scalars().all()
+                return biomes
+            except SQLAlchemyError as e:
+                raise HTTPException(500, "Error getting biomes") from e
 
-    async def update_biome(self, biome_id: int, updated_biome: BiomeCreateSchema) -> BiomeSchema:
+    async def update_biome(
+        self, biome_id: int, updated: BiomeCreateSchema
+    ) -> BiomeSchema:
         async with self.session_factory() as session:
-            result = await session.execute(select(Biome).where(Biome.id == biome_id))
-            biome = result.scalars().first()
-            if not biome:
-                raise ValueError("Biome not found")
+            try:
+                result = await session.execute(
+                    select(Biome).where(Biome.id == biome_id)
+                )
+                biome = result.scalars().first()
+                if not biome:
+                    raise HTTPException(404, "Biome not found")
 
-            for key, value in updated_biome.dict().items():
-                setattr(biome, key, value)
+                for key, value in updated.dict().items():
+                    if hasattr(biome, key):  # Проверяем, что у биома есть такое поле
+                        setattr(biome, key, value)
 
-            await session.commit()
-            await session.refresh(biome)
-            return biome
+                await session.commit()
+                await session.refresh(biome)
+                return biome
+            except SQLAlchemyError as e:
+                await session.rollback()
+                raise HTTPException(500, "Error updating biome") from e
 
     async def delete_biome(self, biome_id: int):
         async with self.session_factory() as session:
-            result = await session.execute(select(Biome).where(Biome.id == biome_id))
-            biome = result.scalars().first()
-            if not biome:
-                raise ValueError("Biome not found")
+            try:
+                result = await session.execute(
+                    select(Biome).where(Biome.id == biome_id)
+                )
+                biome = result.scalars().first()
+                if not biome:
+                    raise HTTPException(404, "Biome not found")
 
-            await session.delete(biome)
-            await session.commit()
+                await session.delete(biome)
+                await session.commit()
+            except SQLAlchemyError as e:
+                await session.rollback()
+                raise HTTPException(500, "Error deleting biome") from e
