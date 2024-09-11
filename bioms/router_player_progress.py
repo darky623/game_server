be1cf586eef324707c968ef22e316770aeffd0de
Vlad_work_oficie <vladamirov@list.ru>
2024-09-11 10:32:25 +0300

    cleaning router

--- bioms/router_player_progress.py
+++ bioms/router_player_progress.py
@@ -2,31 +2,30 @@ from fastapi import APIRouter, Depends
 
 from auth.models import User
 from auth.user_service import get_current_user
-from bioms.biome_service import BiomeService
+
 from bioms.player_progress_service import PlayerProgressService
 from bioms.schemas import PlayerProgressSchema
 from database import AsyncSessionFactory
 
-router = APIRouter(prefix="/player_progress")
+router = APIRouter(prefix="/player_progress/{user_id}")
 service = PlayerProgressService(AsyncSessionFactory)
 
 
-@router.get('/{user_id}', response_model=PlayerProgressSchema)
-async def get_player_progress(user_id: User = Depends(get_current_user)):
-    return await service.get_player_progress(user_id)
+@router.get('', response_model=PlayerProgressSchema)
+async def get_player_progress(user: User = Depends(get_current_user)):
+    return await service.get_player_progress(user.id)
 
 
 @router.post('', response_model=PlayerProgressSchema)
-async def create_player_progress(player_progress: PlayerProgressSchema):
-    return await service.create_player_progress(player_progress)
+async def create_player_progress(user: int = Depends(get_current_user)):
+    return await service.create_player_progress(user.id)
 
 
-@router.patch('/{user_id}', response_model=PlayerProgressSchema)
-async def update_player_progress(user_id: User = Depends(get_current_user),
-                                 player_progress: PlayerProgressSchema = Depends(get_current_user)):
-    return await service.update_player_progress(player_progress)
+@router.patch('', response_model=PlayerProgressSchema)
+async def update_player_progress(update: PlayerProgressSchema):
+    return await service.update_player_progress(update)
 
 
-@router.delete('/{user_id}')
-async def delete_player_progress(user_id: User = Depends(get_current_user)):
-    await service.delete_player_progress(user_id)
+@router.delete('')
+async def delete_player_progress(user: User = Depends(get_current_user)):
+    await service.delete_player_progress(user.id)
