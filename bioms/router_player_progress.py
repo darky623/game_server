42b8ab3138002d82b53aa1bc5ef584909eabdae8
Vlad_work_oficie <vladamirov@list.ru>
2024-09-17 12:43:36 +0300

    pep8 correct

--- bioms/router_player_progress.py
+++ bioms/router_player_progress.py
@@ -1,32 +1,35 @@
-
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from auth.models import User
 from auth.user_service import get_current_user
 
 from bioms.player_progress_service import PlayerProgressService
-from bioms.schemas import PlayerProgressSchema
+from bioms.schemas import PlayerProgressSchema, PlayerProgressUpdateSchema
 from database import AsyncSessionFactory
 
 router = APIRouter(prefix="/player_progress")
 service = PlayerProgressService(AsyncSessionFactory)
 
 
-@router.get('/{user_id}', response_model=PlayerProgressSchema)
+@router.get("/{user_id}", response_model=PlayerProgressSchema)
 async def get_player_progress(user_id: int) -> PlayerProgressSchema:
     return await service.get_player_progress(user_id)
 
 
-@router.post('', response_model=PlayerProgressSchema)
-async def create_player_progress(user: User = Depends(get_current_user)) -> PlayerProgressSchema:
+@router.post("", response_model=PlayerProgressSchema)
+async def create_player_progress(
+    user: User = Depends(get_current_user),
+) -> PlayerProgressSchema:
     return await service.create_player_progress(user.id)
 
 
-@router.patch('/{user_id}', response_model=PlayerProgressSchema)
-async def update_player_progress(update: PlayerProgressSchema) -> PlayerProgressSchema:
+@router.put("/{user_id}", response_model=PlayerProgressUpdateSchema)
+async def update_player_progress(
+    update: PlayerProgressUpdateSchema,
+) -> PlayerProgressSchema:
     return await service.update_player_progress(update)
 
 
-@router.delete('/{user_id}')
+@router.delete("/{user_id}")
 async def delete_player_progress(user_id: int):
     await service.delete_player_progress(user_id)
