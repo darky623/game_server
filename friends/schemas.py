a846b2f4917dce05aa21b017eee1a95e672254ff
Vlad_work_oficie <vladamirov@list.ru>
2024-09-18 16:10:20 +0300

    Create pydantic schemas for Friends

---
+++ friends/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+
+class FriendBase(BaseModel):
+    owner_id: int
+    friend_id: int | None = None
+    chat_id: int | None = None
+    request_to_add_id: int | None = None
+
+
+class FriendCreate(FriendBase):
+    pass
+
+
+class FriendUpdate(FriendBase):
+    pass
+
+
+class RequestToFriendBase(BaseModel):
+    sender_id: int
+    recipient_id: int
+    status: bool = False
+
+
+class RequestToFriendCreate(RequestToFriendBase):
+    pass
+
+
+class RequestToFriendUpdate(RequestToFriendBase):
+    pass
