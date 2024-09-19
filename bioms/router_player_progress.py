f0461d339504bdd7d05c8123cbd704c5d9fb043e
Vlad_work_oficie <vladamirov@list.ru>
2024-09-19 16:42:03 +0300

    dependencies correct

--- bioms/router_player_progress.py
+++ bioms/router_player_progress.py
@@ -11,25 +11,26 @@ router = APIRouter(prefix="/player_progress")
 service = PlayerProgressService(AsyncSessionFactory)
 
 
-@router.get("/{user_id}", response_model=PlayerProgressSchema)
+@router.get("/{user_id}", response_model=PlayerProgressSchema, dependencies=[Depends(get_current_user)])
 async def get_player_progress(user_id: int) -> PlayerProgressSchema:
     return await service.get_player_progress(user_id)
 
 
-@router.post("", response_model=PlayerProgressSchema)
+@router.post("", response_model=PlayerProgressSchema, dependencies=[Depends(get_current_user)])
 async def create_player_progress(
     user: User = Depends(get_current_user),
 ) -> PlayerProgressSchema:
     return await service.create_player_progress(user.id)
 
 
-@router.put("/{user_id}", response_model=PlayerProgressUpdateSchema)
+@router.put("/{user_id}", response_model=PlayerProgressUpdateSchema, dependencies=[Depends(get_current_user)])
 async def update_player_progress(
     update: PlayerProgressUpdateSchema,
 ) -> PlayerProgressSchema:
     return await service.update_player_progress(update)
 
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}", dependencies=[Depends(get_current_user)])
 async def delete_player_progress(user_id: int):
     await service.delete_player_progress(user_id)
+
