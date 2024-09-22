83c7da43f84736dafe35570c47b6031615b16ebc
vfaddey <vfaddey@yandex.ru>
2024-09-22 15:50:02 +0300

    added new routers

--- game_logic/routers/classes_router.py
+++ game_logic/routers/classes_router.py
@@ -1,21 +1,74 @@
-from fastapi import APIRouter, Depends
-from auth.models import User
-from schemas import AddArchetypeSchema, ArchetypeSchema
+from fastapi import APIRouter, Depends, HTTPException
+
 from auth.user_service import get_current_user
+from game_logic.schemas.class_schema import AddCharacterClassSchema, AddCharacterSubclassSchema, CharacterClassSchema, \
+    CharacterSubclassSchema
+from deps import get_services
+from game_logic.models import SummandParams, MultiplierParams, CharacterClass, CharacterSubclass
 
-router = APIRouter(prefix='/archetypes', tags=['archetypes'])
+router = APIRouter(prefix='/classes')
 
 
-@router.post('', response_model=ArchetypeSchema, dependencies=[Depends(get_current_user)])
-async def create_archetype(add_archetype: AddArchetypeSchema):
+@router.post('', response_model=CharacterClassSchema, dependencies=[Depends(get_current_user)])
+async def create_class(add_class: AddCharacterClassSchema,
+                       services = Depends(get_services)):
+    summand_params_model = SummandParams(**add_class.summand_params.model_dump())
+    multiplier_params_model = MultiplierParams(**add_class.multiplier_params.model_dump())
+    inserted_summand_params = await services.params_service.add(summand_params_model)
+    inserted_multiplier_params = await services.params_service.add(multiplier_params_model)
+    character_class_model = CharacterClass(title=add_class.title,
+                                           icon=add_class.icon,
+                                           summand_params_id=inserted_summand_params.id,
+                                           multiplier_params_id=inserted_multiplier_params.id)
+    inserted_class = await services.class_service.add(character_class_model)
+    return inserted_class
+
+
+@router.get('', response_model=list[CharacterClassSchema], dependencies=[Depends(get_current_user)])
+async def get_classes(services = Depends(get_services)):
+    classes = await services.class_service.get_all()
+    return classes
+
+
+@router.get('/{class_id}', response_model=CharacterClassSchema)
+async def get_class_by_id(class_id: int,
+                          services = Depends(get_services)):
+    return await services.class_service.get_by_id(class_id)
+
+
+@router.delete('/{class_id}', dependencies=[Depends(get_current_user)])
+async def delete_class(class_id: int,
+                       services = Depends(get_services)):
     ...
 
 
-@router.get('', response_model=list[ArchetypeSchema], dependencies=[Depends(get_current_user)])
-async def get_archetypes():
-    ...
+@router.post('/{class_id}/subclasses', response_model=CharacterSubclassSchema, dependencies=[Depends(get_current_user)])
+async def create_subclass(class_id: int,
+                          add_subclass: AddCharacterSubclassSchema,
+                          services = Depends(get_services)):
+    existing_class = await services.class_service.get_by_id(class_id)
+    if not existing_class:
+        raise HTTPException(400, detail='No class with such id')
+    summand_params_model = SummandParams(**add_subclass.summand_params.model_dump())
+    multiplier_params_model = MultiplierParams(**add_subclass.multiplier_params.model_dump())
+    inserted_summand_params = await services.params_service.add(summand_params_model)
+    inserted_multiplier_params = await services.params_service.add(multiplier_params_model)
+    character_subclass_model = CharacterSubclass(title=add_subclass.title,
+                                                 icon=add_subclass.icon,
+                                                 multiplier_params_id=inserted_multiplier_params.id,
+                                                 summand_params_id=inserted_summand_params.id)
+    inserted_subclass = await services.class_service.add_subclass(existing_class, character_subclass_model)
+    return inserted_subclass
 
 
-@router.delete('/{archetype_id}', response_model=ArchetypeSchema, dependencies=[Depends(get_current_user)])
-async def delete_archetype(archetype_id: int):
-    ...
+@router.get('/{class_id}/subclasses', response_model=list[CharacterSubclassSchema], dependencies=[Depends(get_current_user)])
+async def get_subclasses_of_class(class_id: int,
+                                  services = Depends(get_services)):
+    return await services.class_service.get_subclasses(class_id)
+
+
+@router.delete('/{class_id}/subclasses/{subclass_id}', dependencies=[Depends(get_current_user)])
+async def delete_subclass(class_id: int, subclass_id: int,
+                          services = Depends(get_services)):
+    await services.class_service.delete_subclass_by_id(class_id, subclass_id)
+    return {'message': 'ok'}
--- main.py
+++ main.py
@@ -7,14 +7,18 @@ from auth.user_service import get_current_user
 from chat.router import router as chat_router
 from chat.websocket import router as chat_websocket_router
 from database import AsyncSessionFactory
-from game_logic.models import CharacterArchetype, Character
+from game_logic.models import Character, CharacterClass
+from game_logic.router import router as game_logic_router
 
 from schemas import CreateCharacterSchema
 
 app = FastAPI()
 
+
 app.include_router(chat_router)
+
 app.include_router(chat_websocket_router)
+app.include_router(game_logic_router)
 
 
 app.add_middleware(
@@ -33,7 +37,7 @@ async def get_archetypes(user: User = Depends(get_current_user)):
         "archetypes": []
     }
     async with AsyncSessionFactory() as db:
-        result = await db.execute(select(CharacterArchetype))
+        result = await db.execute(select(CharacterClass))
         auth_sessions = result.scalars().all()
         for archetype in auth_sessions:
             response['archetypes'].append(archetype.serialize())
