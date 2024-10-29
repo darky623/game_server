e917e86f42cbe0bb53449e7d4ede730917f2c509
Vlad_work_oficie <vladamirov@list.ru>
2024-10-29 15:14:28 +0300

    energy router, correct service class, add edge case

--- src/game_logic/energy/energy_service.py
+++ src/game_logic/energy/energy_service.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
 from sqlalchemy import select
+from starlette.responses import JSONResponse
 
+from config import game_settings
 from config.config import dt_format
 from src.game_logic.energy.models import Energy
 from src.game_logic.energy.schema import EnergySchema, EnergyUpdateSchema
@@ -11,20 +13,30 @@ class EnergyService:
     def __init__(self, session_factory):
         self.session_factory = session_factory
 
-    async def get_energy(self, user_id: int) -> EnergySchema | None:
-        async with self.session_factory() as session:
-            result = await session.execute(select(Energy).where(Energy.user_id == user_id))
-            if result:
-                return EnergySchema.from_orm(result.scalars().first())
-
-    async def update_energy(self, user_id: int, amount: int) -> EnergyUpdateSchema | None:
+    async def get_energy(self, user_id: int) -> EnergySchema:
         """
-        Обновляет или создает энергию у пользователя
-
+        Возвращает энергию пользователя, если ее нет - создаёт энергию
         Args:
             user_id (int): ID пользователя
-            amount (int): Новое количество энергии
+        Returns:
+            EnergySchema: Энергия пользователя
+        """
+        async with self.session_factory() as session:
+            result = await session.execute(select(Energy).where(Energy.user_id == user_id))
+            if not result:
+                # Create energy if it doesn't exist
+                energy = Energy(user_id=user_id, amount=100, last_updated=datetime.now().strftime(dt_format))
+                await session.add(energy)
+                await session.commit()
+                return EnergySchema.from_orm(energy)
+            return EnergySchema.from_orm(result.scalars().first())
 
+    async def update_energy(self, user_id: int, amount: int) -> EnergyUpdateSchema | JSONResponse:
+        """
+        Обновляет или создает энергию у пользователя
+        Args:
+            user_id (int): ID пользователя
+            amount (int): Новое количество энергии(может быть отрицательным)
         Returns:
             EnergyUpdateSchema | None: Обновленная энергия, либо None если энергии не существует
         """
@@ -33,6 +45,10 @@ class EnergyService:
             if energy:
                 # Прибавляем к энергии нужное количество единиц
                 energy.amount += amount
+                if energy.amount < game_settings.energy['energy_min']:
+                    return JSONResponse(status_code=400, content={"message": "Недостаточно энергии"})
+                if energy.amount > game_settings.energy['energy_max']:
+                    energy.amount = game_settings.energy['energy_max']
                 # Обновляем время ласт апдейта энергии
                 current_time = datetime.now().strftime(dt_format)
                 energy.last_updated = current_time
