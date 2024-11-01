72c23a6be92e3f43ec1368eb30bc4c7ea44f20dc
vfaddey <vfaddey@yandex.ru>
2024-11-01 11:38:18 +0300

    initialization of models

---
+++ src/game_logic/models/__init__.py
@@ -0,0 +1,4 @@
+from src.game_logic.models.models import *
+from src.game_logic.models.battle_models import *
+from src.game_logic.models.biome_models import *
+from src.game_logic.models.deck_models import *
