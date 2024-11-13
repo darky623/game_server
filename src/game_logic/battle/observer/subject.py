3acf1c4862681d7d4afc6c248a1033c568a5fefe
vfaddey <vfaddey@yandex.ru>
2024-11-13 16:16:05 +0300

    observer interface

---
+++ src/game_logic/battle/observer/subject.py
@@ -0,0 +1,13 @@
+class Subject:
+    def __init__(self):
+        self.observers = []
+
+    def add_observer(self, observer):
+        self.observers.append(observer)
+
+    def remove_observer(self, observer):
+        self.observers.remove(observer)
+
+    def notify(self, event):
+        for observer in self.observers:
+            observer.update(event)
