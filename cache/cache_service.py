e58d705a506fec1da8bdf25c6dd79020b3a7fa11
vlad Amirov <vladamirov@list.ru>
2024-11-29 00:13:13 +0300

    update cache

--- cache/cache_service.py
+++ cache/cache_service.py
@@ -27,24 +27,30 @@ class CacheService:
         def decorator(func):
             @wraps(func)
             async def wrapper(*args, **kwargs):
-                try:
-                    request: Request = kwargs.get('request')
-                    if request:
-                        key = f"{request.url.path}?{request.query_params}"
-                    else:
-                        key = func.__name__
+                request: Request = kwargs.get('request')
+                if request:
+                    key = f"{request.url.path}?{request.query_params}"
+                else:
+                    key = func.__name__
 
-                    cache_key = hashlib.sha256(key.encode()).hexdigest()
+                cache_key = hashlib.sha256(key.encode()).hexdigest()
 
-                    cached_response = await self.__get_cache(cache_key)
-                    if cached_response:
-                        return cached_response
-                    response = await func(*args, **kwargs)
+                # Проверяем кеш
+                cached_response = await self.__get_cache(cache_key)
+                if cached_response:
+                    return cached_response
+
+                # Получаем новый ответ
+                response = await func(*args, **kwargs)
+
+                # Кешируем ответ
+                if response:
                     if isinstance(response, BaseModel):
                         await self.__set_cache(cache_key, response.model_dump(), ttl)
-                    if isinstance(response, list) and isinstance(response[0], BaseModel):
+                    elif isinstance(response, list) and response and isinstance(response[0], BaseModel):
                         await self.__set_cache(cache_key, [schema.model_dump() for schema in response], ttl)
-                finally:
-                    return response
+
+                return response
+
             return wrapper
         return decorator
